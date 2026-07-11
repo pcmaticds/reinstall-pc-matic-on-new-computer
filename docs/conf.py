5546b5a046236e5d84f6a4931285d460f01e9ac7
pcmaticds <kjhgdhu@outlook.com>
2026-07-11 14:14:56 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,21 @@
+project = 'How-to-Reinstall-Pc-Matic-On-a-new-Computer'
+author = 'How-to-Reinstall-Pc-Matic-On-a-new-Computer'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
+
